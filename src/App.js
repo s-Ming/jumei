6f1646fa60148d8125ab7b6ec95acad18fc69870
lily <477800458@qq.com>
2018-12-03 18:09:42 +0800

    ..

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+import {Route} from "react-router-dom";
+
+import Index from './pages/Home.jsx';
+// import Mine from "./pages/mine/Mine.jsx";
+
+class App extends Component {
+    render(){
+        retrun ()
+        
+    }
+}
